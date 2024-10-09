450df8f2e75720b00486f8976ae584ca2f079106
Ritzl <benceritzl@gmail.com>
2024-10-09 08:50:05 +0200

    09-27

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { DriversComponent } from './drivers/drivers.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+const routes: Routes = [
+ {path:"home", component:HomeComponent},
+ {path:"drivers", component:DriversComponent},
+ {path:"", redirectTo:"drivers", pathMatch:"full"},
+ {path:"**", component:ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { DriversComponent } from './drivers/drivers.component';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from './search.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ErrorPageComponent,
+    DriversComponent,
+    SearchPipe,
+    SortPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
